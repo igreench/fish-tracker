819b733da47dd13c9cbc7c7d07635ec2a11c0754
igreench <eugenejsm@mail.ru>
2014-12-29 19:28:15 +0700

    initUndistortRectifyMap and remap

--- main.cpp
+++ main.cpp
@@ -66,6 +66,24 @@ int main() {
         cv::Mat image = cv::Mat(rgbImage.GetRows(), rgbImage.GetCols(), CV_8UC3, rgbImage.GetData(),rowBytes);
 
         cv::imshow("image", image);
+
+        //try to undistort
+        cv::Mat rmap[2];
+        cv::Mat cameraMatrix = (cv::Mat_<double>(3,3) << 1898.86,0,801.607,0,1684.22,533.037,0,0,1);
+        //3,3;1898.86,0,801.607;0,1684.22,533.037;0,0,1;
+        cv::Mat distCoeffs = (cv::Mat_<double>(1,5) << -0.138546,-0.290375,0.00790141,0.00869189,0);
+        //1,5;-0.138546,-0.290375,0.00790141,0.00869189,0;
+        cv::Mat R1, P1, rimage;
+        cv::Size imageSize = image.size();
+        //R1 = cameraMatrix.inv()*H1*cameraMatrix;//
+        //P1 = cameraMatrix;//
+        cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, R1, P1, imageSize, CV_16SC2, rmap[0], rmap[1]);
+        //cv::initUndistortRectifyMap(intrinsic, distortion, R, P, CV_8UC3 mapx, mapy);
+        cv::remap(image, rimage, rmap[0], rmap[1], CV_INTER_LINEAR);
+
+        cv::imshow("image1", rimage);
+        //
+
         key = cv::waitKey(30);
     }
 
@@ -75,6 +93,10 @@ int main() {
         // This may fail when the camera was removed, so don't show
         // an error message
     }
+
+    //intristics 3,3;1977,0,934.867;0,1812.95,904.821;0,0,1;
+    //distCoeffs 1,5;-0.0833973,0.151344,0.0536747,0.045857,0;
+
 
     camera.Disconnect();
 
